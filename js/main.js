f3c40a0674012a6909b0efc15a5bb2d7804b1307
ludo <ludovic.peysson@laposte.net>
2023-08-01 16:54:57 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,43 +1,69 @@
-let size = 20;
-let labyrinthe1 = labyrinthes[size]["ex-0"];
-let labyrinthe2 = labyrinthes[size]["ex-1"];
-let labyrinthe3 = labyrinthes[size]["ex-2"];
-let labyrinthe4 = labyrinthes[size]["ex-0"];
-let labyrinthe20 = labyrinthes[size]["ex-2"];
+let sizeSelected = 3;
+let typeSelected = 0;
 
-console.log(labyrinthe20);
+init()
+createOptions();
 
-let lab = document.getElementById('labyrinthe');
-let entrance = document.getElementById('entrance');
-
-for(i=0;i<size*size;i++){
-    let div = document.createElement('div')
-    lab.appendChild(div);
-    div.classList.add("cell");
-    div.classList.add("child");
-    div.setAttribute("id", i);
-
-    if(labyrinthe20[i].hasOwnProperty('entrance')){
-        div.classList.add('depart');
+function createOptions(){
+    for(let labSize=4; labSize<26; labSize++){
+        let option = document.createElement('option');
+        let sizeSelector = document.getElementById('labyrinthe_size');
+        option.textContent = labSize;
+        option.setAttribute("value", labSize);
+        sizeSelector.appendChild(option);
     }
-    if(labyrinthe20[i].hasOwnProperty('exit')){
-        div.classList.add('sortie');
+    for(let labVariant=1; labVariant<3; labVariant++){
+        let option = document.createElement('option');
+        let typeSelector = document.getElementById('typeOfLab');
+        option.textContent = labVariant;
+        option.setAttribute("value", labVariant);
+        typeSelector.appendChild(option);
     }
-
-    if(labyrinthe20[i].walls[0]==true){
-        div.classList.add('topWall')
-    }
-    if(labyrinthe20[i].walls[1]==true){
-        div.classList.add('rightWall')
-    }
-    if(labyrinthe20[i].walls[2]==true){
-        div.classList.add('bottomWall')
-    }
-    if(labyrinthe20[i].walls[3]==true){
-        div.classList.add('leftWall')
-    }
-        // labyrinthe1[i].walls.map(div.classList(()))
 }
+
+
+function init(){
+    let exArray = ["ex-0", "ex-1", "ex-2"];
+    console.log(sizeSelected);
+    console.log(exArray[typeSelected]);
+    labyrinthe = labyrinthes[sizeSelected][exArray[typeSelected]];
+    let lab = document.getElementById('labyrinthe');
+    lab.innerHTML = "";
+
+    for( let cell of labyrinthe){
+        let div = document.createElement('div');
+        div.id = cell.posX + "-" + cell.posY;
+        div.classList.add("cell", "child");
+        div.setAttribute("style", `flex-basis : ${100/sizeSelected}%`);  
+
+        if( cell.entrance ){
+            div.classList.add('depart');
+        }
+        if(cell.hasOwnProperty('exit')){
+            div.classList.add('sortie');
+        }
+
+        let border = ""
+        cell.walls.forEach(wallBool => wallBool? border += "6px " : border += "0px ");
+        div.style.border = "ridge black";
+        div.style.borderWidth = border;
+        console.log(border);
+        lab.appendChild(div);
+    }
+}
+
+let labyrintheSize = document.getElementById('labyrinthe_size');
+let labyrintheType = document.getElementById('typeOfLab');
+labyrintheSize.addEventListener('change', function(){
+    sizeSelected = labyrintheSize.value;
+    init();
+});
+labyrintheType.addEventListener('change', function(){
+    typeSelected = labyrintheType.value;
+    init();
+});
+
+
 
 
 
